af8c502fd608c863a516cf124fb443a455a1a3c5
mikiBrill <33099023+mikiBrill@users.noreply.github.com>
2018-01-09 17:00:14 +0200

    Testing push, take 2

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,25 @@
-Parse.Cloud.define('hello', function(req, res) {
-   res.success('Hi');
- });
+// Defined in cloud/main.js on Parse server side
+Parse.Cloud.define('pingReply', function(request, response) {
+  var params = request.params;
+  var customData = params.customData;
+
+  if (!customData) {
+    response.error("Missing customData!")
+  }
+
+  var sender = JSON.parse(customData).sender;
+  var query = new Parse.Query(Parse.Installation);
+  query.equalTo("installationId", sender);
+
+  Parse.Push.send({
+  where: query,
+  // Parse.Push requires a dictionary, not a string.
+  data: {"alert": "The Giants scored!"},
+  }, { success: function() {
+     console.log("#### PUSH OK");
+  }, error: function(error) {
+     console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true});
+
+  response.success('success');
+});
