ee1250c87906c613d2095f4daaed0fced3b34b0d
mikiBrill <33099023+mikiBrill@users.noreply.github.com>
2018-01-09 16:31:43 +0200

    Revert back to origin

--- cloud/main.js
+++ cloud/main.js
@@ -1,25 +1,3 @@
-// Defined in cloud/main.js on Parse server side
-Parse.Cloud.define('pingReply', function(request, response) {
-  var params = request.params;
-  var customData = params.customData;
-
-  if (!customData) {
-    response.error("Missing customData!")
-  }
-
-  var sender = JSON.parse(customData).sender;
-  var query = new Parse.Query(Parse.Installation);
-  query.equalTo("installationId", sender);
-
-  Parse.Push.send({
-  where: query,
-  // Parse.Push requires a dictionary, not a string.
-  data: {"alert": "The Giants scored!"},
-  }, { success: function() {
-     console.log("#### PUSH OK");
-  }, error: function(error) {
-     console.log("#### PUSH ERROR" + error.message);
-  }, useMasterKey: true});
-
-  response.success('success');
-});
+Parse.Cloud.define('hello', function(req, res) {
+   res.success('Hi');
+ });
