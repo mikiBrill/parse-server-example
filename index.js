bf41dd8ad7713b6afb067d09830e9d705e923436
mikiBrill <33099023+mikiBrill@users.noreply.github.com>
2018-01-09 16:56:14 +0200

    push notifications setup, take 2

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  },
+  push: {
+    android: {
+      senderId: process.env.GCM_SENDER_ID,
+      apiKey: process.env.GCM_API_KEY
+    }
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
