213221f090dd1dce4947fbabe523c49b80666bed
mikiBrill <33099023+mikiBrill@users.noreply.github.com>
2018-01-09 15:03:49 +0200

    Added senderId & apiKey for push notifications

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
+  push: {
+    android: {
+      senderId: '395232922299',
+      apiKey: 'AIzaSyAU--0NKCfxqbhhfS_InjbZp-Jw2pBqv8Q'
+    }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
