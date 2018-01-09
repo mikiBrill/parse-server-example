9b0d358d2136e0f7baca16d0f634ccf7b8452af2
mikiBrill <33099023+mikiBrill@users.noreply.github.com>
2018-01-09 16:46:59 +0200

    revert back to original

--- index.js
+++ index.js
@@ -19,12 +19,7 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  },
-  push: {
-    android: {
-      senderId: process.env.GCM_SENDER_ID,
-      apiKey: process.env.GCM_API_KEY
-    }
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
