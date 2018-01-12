4444ccf95b2a3a4fb1b4c368c590067844999dd7
mikiBrill <33099023+mikiBrill@users.noreply.github.com>
2018-01-12 19:25:33 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,25 +1,25 @@
-// Defined in cloud/main.js on Parse server side
 Parse.Cloud.define('pingReply', function(request, response) {
-  var params = request.params;
-  var customData = params.customData;
+	var params = request.params;
+	var customData = params.customData;
 
-  if (!customData) {
-    response.error("Missing customData!")
-  }
+	if (!customData) {
+		response.error("Missing customData!")
+	}
 
-  var sender = JSON.parse(customData).sender;
-  var query = new Parse.Query(Parse.Installation);
-  query.equalTo("installationId", sender);
+	var jsonData = JSON.parse(customData);
+	var sender = jsonData.sender;
+	var query = new Parse.Query(Parse.Installation);
+	query.equalTo("installationId", sender);
 
-  Parse.Push.send({
-  where: query,
-  // Parse.Push requires a dictionary, not a string.
-  data: {"message":"hello phone"},
-  }, { success: function() {
-     console.log("#### PUSH OK");
-  }, error: function(error) {
-     console.log("#### PUSH ERROR: " + error.message);
-  }, useMasterKey: true});
+	Parse.Push.send({
+		where: query,
+		// Parse.Push requires a dictionary, not a string.
+		data: {"message": "hello phone"},
+	}, { success: function() {
+		console.log("#### PUSH OK");
+	}, error: function(error) {
+		console.log("#### PUSH ERROR: " + error.message);
+	}, useMasterKey: true});
 
-  response.success('success');
+	response.success('success');
 });
