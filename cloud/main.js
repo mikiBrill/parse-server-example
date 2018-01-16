53488016119d8e3e5e553000250c1a85b1154e7a
mikiBrill <33099023+mikiBrill@users.noreply.github.com>
2018-01-16 13:14:59 +0200

    Added push to restaurant too

--- cloud/main.js
+++ cloud/main.js
@@ -8,9 +8,12 @@ Parse.Cloud.define('notifyClient', function(request, response) {
 
 	var jsonData = JSON.parse(customData);
 	var clientInstallationId = jsonData.client;
+	var restInstallationId = jsonData.rest;
 	var restaurantName = jsonData.restaurant;
 	var reservationId = jsonData.reservationId;
-	var msg = jsonData.message;
+	var msg = jsonData.clientMessage;
+	var restMsg = jsonData.restMessage;
+	
 	var query = new Parse.Query(Parse.Installation);
 	query.equalTo("installationId", clientInstallationId);
 
@@ -21,9 +24,22 @@ Parse.Cloud.define('notifyClient', function(request, response) {
 		       "restName": restaurantName,
 		       "resvId"	 : reservationId},
 	}, { success: function() {
-		console.log("#### PUSH OK");
+		console.log("#### CLIENT PUSH OK");
 	}, error: function(error) {
-		console.log("#### PUSH ERROR: " + error.message);
+		console.log("#### CLIENT PUSH ERROR: " + error.message);
+	}, useMasterKey: true});
+	
+	var query = new Parse.Query(Parse.Installation);
+	query.equalTo("installationId", restInstallationId);
+
+	Parse.Push.send({
+		where: query,
+		// Parse.Push requires a dictionary, not a string.
+		data: {"alert"	 : restMsg},
+	}, { success: function() {
+		console.log("#### RESTAURANT PUSH OK");
+	}, error: function(error) {
+		console.log("#### RESTAURANT PUSH ERROR: " + error.message);
 	}, useMasterKey: true});
 
 	response.success('success');
