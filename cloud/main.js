6eba3829e33de096fd9f3ab5555459fac0c8a313
mikiBrill <33099023+mikiBrill@users.noreply.github.com>
2018-01-24 21:00:13 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -136,7 +136,6 @@ Parse.Cloud.define('deleteFiles', function(request, response) {
 	var MongoClient = require('mongodb').MongoClient;
 	var url = "mongodb://admin:admin@ds129906.mlab.com:29906/heroku_tjh6fmn7";
 	MongoClient.connect(url, function(err, db) {
-		assert.equal(null, err);
 		console.log("Connected successfully to server");
 		db.close();
 	});
